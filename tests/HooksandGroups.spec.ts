2a1975aa5145f2e89fbaeeb440fde3cf361b2a74
Kaushik Srinivasan <ksrinivasan@6river.com>
2023-08-17 21:58:14 -0400

    Fixed bugs

--- tests/HooksandGroups.spec.ts
+++ tests/HooksandGroups.spec.ts
@@ -2,32 +2,36 @@ import { test, expect, Page } from "@playwright/test";
 
 let page: Page;
 
-test.beforeEach(async ({ browser }) => {
-  page = await browser.newPage();
-  await page.goto("https://www.saucedemo.com/");
-  //   await page.pause();
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill("standard_user");
-  await page.locator('[data-test="username"]').press("Tab");
-  await page.locator('[data-test="password"]').fill("secret_sauce");
-  await page.locator('[data-test="password"]').press("Enter");
-});
+test.describe("All Hooks and Groups tests", () => {
+  test.beforeEach(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto("https://www.saucedemo.com/");
+    //   await page.pause();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("standard_user");
+    await page.locator('[data-test="username"]').press("Tab");
+    await page.locator('[data-test="password"]').fill("secret_sauce");
+    await page.locator('[data-test="password"]').press("Enter");
+  });
 
-test.afterAll(async () => {
-  await page.close();
-});
+  test.afterAll(async () => {
+    await page.close();
+  });
 
-test("homepage", async () => {
-  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-  await page.locator("#item_1_title_link").click();
-  await page
-    .locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]')
-    .click();
-  await page.locator('[data-test="back-to-products"]').click();
-});
+  test("homepage", async () => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page
+      .locator('[data-test="add-to-cart-sauce-labs-bike-light"]')
+      .click();
+    await page.locator("#item_1_title_link").click();
+    await page
+      .locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]')
+      .click();
+    await page.locator('[data-test="back-to-products"]').click();
+  });
 
-test("logout", async () => {
-  await page.getByRole("button", { name: "Open Menu" }).click();
-  await page.getByRole("link", { name: "Logout" }).click();
+  test("logout", async () => {
+    await page.getByRole("button", { name: "Open Menu" }).click();
+    await page.getByRole("link", { name: "Logout" }).click();
+  });
 });
